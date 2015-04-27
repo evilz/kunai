3f8dddb0d7e25ef05e6c8a0c1fbf96675daf13e0
evilz <evilznet@gmail.com>
2015-04-27 19:09:52 +0200

    init commit
    create by ProjectScaffold

---
+++ src/Kunai/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Kunai")>]
+[<assembly: AssemblyProductAttribute("Kunai")>]
+[<assembly: AssemblyDescriptionAttribute("kunai4ninja")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
